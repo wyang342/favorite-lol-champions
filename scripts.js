e474566c5c9f2a007d7851fa292a4c27d57e06ff
Won Yang <won@gatech.edu>
2020-12-25 22:39:28 -0600

    added D3 to repo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-const inputField = document.getElementById('input')
-const searchButton = document.getElementById('search')
-searchButton.addEventListener('click', e=> {
-	console.log('You clicked search')
-	console.log(inputField.value)
-})
+// Selecting DOM Elements
+const inputSummoner = document.getElementById('inputSummoner');
+const inputAPI = document.getElementById('inputAPIKey');
+const searchButton = document.getElementById('search');
+
+// Initializing variables
+var summonerName, apiKey;
+
+// Defining API Endpoints
+const baseURL = 'https://na1.api.riotgames.com';
+const getSummonerByName = baseURL + "/lol/summoner/v4/summoners/by-name/" + summonerName + 
+
+// Defining functions
+function searchListener() { // should probably change function name to listenforsearch
+	searchButton.addEventListener('click', e => {
+		summonerName = inputSummoner.value;
+		apiKey = inputAPI.value;
+	});
+}
+
+
+// Initializing functions
+searchListener();
+
